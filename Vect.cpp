9d5d5d2d8919d4d178169c1b99771bab8be0916c
XavZol <alansolis707@gmail.com>
2025-08-01 05:56:48 -0600

    Se gizo un programa de vectores con 10 letras del abecedario

--- Vect.cpp
+++ Vect.cpp
@@ -1,25 +1,25 @@
 #include<iostream>
-#include "Vect.h"
 #include<conio.h>  
 using namespace std;
 int main(){
-//Escribe un programa defina un vector de numeros y calcule si existe algun numero en el vectoor cuyo valor equivale a la suma del resto de numeros del vector.
-int numero[5] = {1, 2, 3, 4, 10};
-int suma = 0, mayor = 0;
+// Escriba un programa que defina dos vectores de caracteres, despues almacene el contenido de ambos vectores en un nuevo vector, situando en el primer lugar 
+// los elementos del segundo vector. muestre el contenidoo del nuevo vector en la salida estandar.
+char letras[]={'a','b','c','d','e'};
+char numero[]=['1','2','3','4','5'];
+char nuevo[10];
+// copiando los elemtnos de elmentos de letras a nuevo 
 for(int i=0; i<5; i++){
-    suma += numero[i];
-
-    if (numero [i] >mayor){
-        mayor = numero[i];
+    nuevo[i]  = letras[i];{
     }
-}   
-if(mayor == suma-mayor){
-    cout<<"El numero"<<mayor<<" equivale a suma de los demas";
+    for (int i=0; i<10; i++){
+        nuevo[i]= numero[i-5];
+    }
+    //mostrar el nuevo vector
+    for(int i=0; i<10; i++){
+        cout<<nuevo[i]<<" ";
+    }
 }
-else{
-    cout<<"No existe ningun numero que sea igual a la suma de los demas"<<endl;
-}
-cout<<"La suma de los numeros es: " << suma << endl;
+
 getch();
     return 0;
 }
