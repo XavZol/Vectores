fae06f19dabbdec07b225c8ba956f1a0350c8546
XavZol <alansolis707@gmail.com>
2025-08-01 06:58:00 -0600

    Programa vectores bidimensional mostrando diagonal principal

--- Vect.cpp
+++ Vect.cpp
@@ -2,24 +2,21 @@
 #include<conio.h>  
 using namespace std;
 int main(){
-// Matrices 
-// Hacer un programa para rellenar una matriz pidiendo al usuario el numero de filas y columnas; posteriormente mostrar la matriz en pantalla;
-int main[100][100], filas, columnas;
-cout<<"Digite el numero de filas: ";
-cin>>filas;
-cout<<"Digite el numero de columnas; ";
-cin>>columnas;
-//almacenando numeros en la matriz
-for (int i=0; i<filas; i++){
-    for (int j=0; j<columnas; j++){
-        cout<<"Digite el valor de la posicion ["<<i<<"]"<<"["<<j<<"]";
-        cin>>numero[i][j];
+// Realiza un programa que defina una matriz  de 3x3  y escriba un ciclo para que muestre la diagonal principal de la matriz.
+int numeros[3][3] = {{1,2,3}{4,5,6}{7,8,9}};
+for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+        cout<<numeros[i][j];
     }
-}
-for (int i=0; i<filas; i++){
-    for (int j=0; j<columnas; j++){
-        cout<<numeros[i][j]<<" ";
+    cout<<"Mostrando diagonal principal:\n";
+    for (int i=0; i<3; i++){
+        for (int j=0; j<3; j++){
+            if(i==j){
+                cout<<"numeros"[i][j]<<endl;
+            }
+        }
     }
+
 }
 getch();
     return 0;
