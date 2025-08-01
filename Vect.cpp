108dc6ffd0adb76f8a64a52098df4bb5ac23a797
XavZol <alansolis707@gmail.com>
2025-08-01 06:10:04 -0600

    Programa de vectorees de multiplicacion por dos los vectores

--- Vect.cpp
+++ Vect.cpp
@@ -2,24 +2,21 @@
 #include<conio.h>  
 using namespace std;
 int main(){
-// Escriba un programa que defina dos vectores de caracteres, despues almacene el contenido de ambos vectores en un nuevo vector, situando en el primer lugar 
-// los elementos del segundo vector. muestre el contenidoo del nuevo vector en la salida estandar.
-char letras[]={'a','b','c','d','e'};
-char numero[]=['1','2','3','4','5'];
-char nuevo[10];
-// copiando los elemtnos de elmentos de letras a nuevo 
+// Hacer un programa que lea 5 numeros en un arreglo, le copie a otro arreglo multiplicado por dos y muestre el segundo arreglo.
+int numeros[5], numeros2[5];
 for(int i=0; i<5; i++){
-    nuevo[i]  = letras[i];{
-    }
-    for (int i=0; i<10; i++){
-        nuevo[i]= numero[i-5];
-    }
-    //mostrar el nuevo vector
-    for(int i=0; i<10; i++){
-        cout<<nuevo[i]<<" ";
-    }
+    cout<<"Digite los elementos del arreglo:\n";
+    cin>>numeros[i];
 }
-
+// Multiplicar por los 2 numeros n del areglo
+for(int i=0; i<5; i++){
+    numeros2[i] = numeros[i] * 2;
+}
+// Mostrar el segundo arreglo
+cout<<"El segundo arreglo es:\n";
+for (int i=0; i<5; i++){
+    cout<<numeros2[i]<<"\n";
+}
 getch();
     return 0;
 }
