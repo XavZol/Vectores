441b71b11e33e673dbbbdcd13a9a08d2a0e24579
XavZol <alansolis707@gmail.com>
2025-08-01 18:45:12 -0600

    Programa de una matriz de tipo entero 2+2

--- Vect.cpp
+++ Vect.cpp
@@ -1,23 +1,24 @@
 #include<iostream>
-#include<conio.h>  
+#include<stdlib.h>  
 using namespace std;
 int main(){
-// Realiza un programa que defina una matriz  de 3x3  y escriba un ciclo para que muestre la diagonal principal de la matriz.
-int numeros[3][3] = {{1,2,3}{4,5,6}{7,8,9}};
-for(int i=0; i<3; i++){
-    for(int j=0; j<3; j++){
-        cout<<numeros[i][j];
-    }
-    cout<<"Mostrando diagonal principal:\n";
-    for (int i=0; i<3; i++){
-        for (int j=0; j<3; j++){
-            if(i==j){
-                cout<<"numeros"[i][j]<<endl;
-            }
-        }
-    }
+// Hacer una matriz de tipo entero de 2+2, Llenarla de numeros y luego copiar todo su contenido hacia otra matriz.
+int numeros[2][2] = {{1,2},{3,4}};
+int matriz[2][2]; 
 
+for (int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+        matriz[i][j]=numeros[i][j];
+    }
 }
-getch();
+// Mostrando matriz 
+for (int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+        cout<<matriz[i][j];
+    }
+    cout<<" "\n;
+}
+system("pause");
+
     return 0;
 }
