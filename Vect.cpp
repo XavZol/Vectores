25aca33a11da4a1c4315899fde3ad072e6492697
XavZol <alansolis707@gmail.com>
2025-08-01 19:09:29 -0600

    Pidiendole al usuario numero de filas y columnas

--- Vect.cpp
+++ Vect.cpp
@@ -1,24 +1,39 @@
 #include<iostream>
-#include<stdlib.h>  
+#include<stdlib.h>
+#include<time.h>
+#include<conio.h>  
 using namespace std;
 int main(){
-// Hacer una matriz de tipo entero de 2+2, Llenarla de numeros y luego copiar todo su contenido hacia otra matriz.
-int numeros[2][2] = {{1,2},{3,4}};
-int matriz[2][2]; 
+// Hacer una matriz pidiendole al usuario de numero de filas y columnas, llenar de numeros aleatorios, copiar el contenido a otra matriz y por ultima mostrarla en pantalla.
+int numeros[100][100], dato, filas, columnas;
+int numeros2[100][100];
+cout<<"Digite el numero de filas:\n";
+cin>>filas;
+cout<<"Digite el numero de columnas:\n";
+cin>>columnas;
 
-for (int i=0; i<2; i++){
-    for(int j=0; j<2; j++){
-        matriz[i][j]=numeros[i][j];
+srand(time(NULL)); //generar numeros aleatorios
+
+for(int i=0; i<filas; i++){
+    for(int j=0; j<columnas; j++){
+        dato = i+rand()%(100); // generando numeros aleatorios (1-100)
+    numero[i][j] = dato;
     }
 }
-// Mostrando matriz 
-for (int i=0; i<2; i++){
-    for(int j=0; j<2; j++){
-        cout<<matriz[i][j];
+// Copiando el contenido a otra matriz 
+for(int i=0; i<filas; i++){
+    for(int j=0; j<columnas; i++){
+        numeros2[i][j] = numeros[i][j];
     }
-    cout<<" "\n;
+}
+// Imprimiendo matriz numero 2 en pantalla
+for(int i=0; i<filas; i++){
+    for(int j=0; j<columnas; i++){
+        cout<<numeros2[i][j]<<" ";
+    }
+    cout<<"\n";
 }
 system("pause");
-
+getch();
     return 0;
 }
