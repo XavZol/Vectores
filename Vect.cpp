5e8170fdad554b0742806bc7fb28ada2b4d42df0
XavZol <alansolis707@gmail.com>
2025-08-03 07:39:38 -0600

    Suma de dos matrices cuadradas

--- Vect.cpp
+++ Vect.cpp
@@ -4,29 +4,30 @@
 #include<conio.h>  
 using namespace std;
 int main(){
-// Realice un programa que lea una matriz de 3x3 y cree su matriz traspuesta. La matriz traspuesta es aquella en la que la columna 1 era la fila 1 de la matriz original.
-/1 2 3/     /1 4 7/
-/4 5 6/     /2 5 8/ 
-/7 8 9/     /3 6 9/
-int numeros[3][3];
-for(int i=0; i<3; i++){
+// Realice un programa que calcule la suma de dos matrices cuadradas de 3x3.
+
+int numeros[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+int numeros2[3][3] = {{9,8,7},{6,5,4},{3,2,1}};
+
+for(int i=0; I<3; i++){
     for(int j=0; j<3; j++){
-        cout<<"Digite un numero ["<<i<<"]["<<j<<"]:";
-        cin>>numeros[i][j];
-    }
-}
-cout<<"Matriz Original\n";
-for(int i=0; i<3; i++){
-    for(int j=0; j<3; j++){
-        cout<<numeros[i][j];
+        cout<<numeros[i][j]<<" ";
     }
     cout<<"\n";
 }
-cout<<"\nMatriz Transpuesta\n";
+
+for(int i=0; I<3; i++){
+    for(int j=0; j<3; j++){
+        cout<<numeros2[i][j]<<" ";
+    }
+    cout<<"\n";
+}
+cout<<"Suma de las dos matrices:\n";
 for(int i=0; i<3; i++){
     for(int j=0; j<3; j++){
-        cout<<numeros[j][i];
+        cout<<numeros[i][j] + numeros2[i][j]<<" ";
     }
+    cout<<"\n";
 }
 system("pause");
 getch();
